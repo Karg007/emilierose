7cf97d35a30088c61f882ddbc08d5da99274e1dc
Karg007 <69087443+Karg007@users.noreply.github.com>
2026-03-02 18:14:48 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import Typewriter from "../components/Typewriter";
+import Typewriter from "./components/Typewriter";
 
 export default function PhotoPage() {
   return (
