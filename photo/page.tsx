c5de3d3ba331553ef6db8eb0a0471b5193b33d27
Karg007 <69087443+Karg007@users.noreply.github.com>
2026-03-03 15:01:06 -0500

    Add files via upload

---
+++ photo/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+import Typewriter from "../components/Typewriter";
+export default function PhotoPage() {
+  return (
+    <main>
+      <h1><Typewriter text="ÉMILIE ROSE" speed={45} /></h1>
+    </main>
+  );
+}
