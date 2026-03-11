6282cc90de23b68ba6e7d5a2bf97b390352f3bb7
Karg007 <69087443+Karg007@users.noreply.github.com>
2026-03-11 16:02:08 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,30 @@
+import localFont from "next/font/local";
 import "../styles/globals.css";
+
+const moonscapeThin = localFont({
+  src: "../public/fonts/Moonscape Thin TTF.ttf",
+  variable: "--font-moonscape",
+  display: "block",
+});
+
+const orbit = localFont({
+  src: "../public/fonts/Orbit-Regular.ttf",
+  variable: "--font-orbit",
+  display: "block",
+});
 
 export const metadata = {
   title: "Émilie Rose",
   description: "Photographie & Art",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <html lang="fr-CA">
+    <html lang="fr" className={`${moonscapeThin.variable} ${orbit.variable}`}>
       <body>{children}</body>
     </html>
   );
